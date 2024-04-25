56eda621742c19389dca6c5a151c6933547bde61
Logesh-KS <113246318+Logesh-KS@users.noreply.github.com>
2024-04-25 18:12:41 +0530

    Update Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,8 +1,10 @@
-module fa_ha(a,b,c,sum,carry);
+module fulladder(a,b,c,sum,carry);
 input a,b,c;
 output sum,carry;
-
-
-
-
+wire w1,w2,w3;
+xor g1(sum,a,b,c);
+and g2(w1,a,b);
+and g3(w2,b,c);
+and g4(w3,c,a);
+or g5(carry,w1,w2,w3);
 endmodule
