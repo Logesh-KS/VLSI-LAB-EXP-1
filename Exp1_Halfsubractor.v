cf934dc958538fb35776ba8ea00ffcdb3df1587b
Logesh-KS <113246318+Logesh-KS@users.noreply.github.com>
2024-04-25 18:15:56 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,8 @@
-module hs(a,b,difference,borrow);
+module halfsubtractor(a,b,d,bo);
 input a,b;
-output difference,borrow;
-
-
-
+wire w1;
+output d,bo;
+xor g1(d,a,b);
+not g2(w1,a);
+and g3(bo,w1,b);
 endmodule
