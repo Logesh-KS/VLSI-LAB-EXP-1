b2a8c25b8551c680ac33e0fd9eebceeb36926ed7
Logesh-KS <113246318+Logesh-KS@users.noreply.github.com>
2024-04-25 18:13:16 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,10 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
+module full_sub(a,b,c,d,bout);
+input a,b,c;
+wire w1,w2,w3;
 output d,bout;
-
-
-
+xor g1(w1,a,b);
+xor g2(d,w1,c);
+and g3(w2,~a,b);
+and g4(w3,~w1,c);
+or g5(bout,w2,w3);
 endmodule
