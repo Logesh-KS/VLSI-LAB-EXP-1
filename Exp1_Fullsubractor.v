edcf91377a7511cd3b63953e880e66e037ac0d16
Logesh-KS <113246318+Logesh-KS@users.noreply.github.com>
2024-04-28 14:53:15 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,10 +1,7 @@
-module full_sub(a,b,c,d,bout);
-input a,b,c;
-wire w1,w2,w3;
-output d,bout;
-xor g1(w1,a,b);
-xor g2(d,w1,c);
-and g3(w2,~a,b);
-and g4(w3,~w1,c);
-or g5(bout,w2,w3);
-endmodule
+
+module full_subtractor(a, b, c,D, Bout);
+input a, b, c;
+output D, Bout;
+  assign D = a ^ b ^ c;
+  assign Bout = (~a & b) | (~(a ^ b) & c);
+endmodule  
