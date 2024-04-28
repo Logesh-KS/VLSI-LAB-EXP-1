922e5ad1131ddea6b70c8f486d1e876bf4b1b307
Logesh-KS <113246318+Logesh-KS@users.noreply.github.com>
2024-04-28 14:52:58 +0530

    Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,10 +1,8 @@
-module fulladder(a,b,c,sum,carry);
+
+module full_adder(sum,cout,a,b,c);
 input a,b,c;
-output sum,carry;
-wire w1,w2,w3;
-xor g1(sum,a,b,c);
-and g2(w1,a,b);
-and g3(w2,b,c);
-and g4(w3,c,a);
-or g5(carry,w1,w2,w3);
+output sum,cout; 
+    assign sum = (a ^ b ^ c );
+    assign cout = (a & b ) | (b & c) | (a & c);
+    
 endmodule
